e4cc2f422b2f8bad5e4afa2f915c21c04466db75
Ahadu Kebede <ahadukebede@gmail.com>
2024-11-12 01:10:21 -0500

    Conditional `NowPlaying` render

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import './App.css';
-import NowPlaying from './NowPlaying/NowPlaying';
-import Button from './Button/Button';
+import "./App.css";
+import { useEffect } from "react";
+import NowPlaying from "./NowPlaying/NowPlaying";
+import Button from "./Button/Button";
+import { useCallback } from "react";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-    <NowPlaying />
-    <Button text="Logout" />
-    </div>
-  );
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	const fetchAuthStatus = useCallback(async () => {
+		try {
+			const response = await fetch("http://localhost:8080/auth_status");
+			const data = await response.json();
+			setIsAuthenticated(data.data);
+			console.log("got");
+		} catch (error) {
+			console.error("Error fetching auth status:", error);
+			setIsAuthenticated(false);
+		}
+	}, []);
+
+	useEffect(() => {
+		fetchAuthStatus();
+	}, []);
+
+	return (
+		<div className="App">
+			{isAuthenticated ? (
+        // This only shows if someone is authenticated
+				<div className="nowplaying">
+					<NowPlaying />
+					<Button text="Logout" />
+				</div>
+			) : (
+        // This only shows if no one is authenticated.
+
+        // TODO: Make a takeover component.
+				<div></div>
+			)}
+		</div>
+	);
 }
 
 export default App;
