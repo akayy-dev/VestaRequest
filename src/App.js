68f98fae52330e66dbe9f3fcfabfed6e9b283eb8
Ahadu Kebede <ahadukebede@gmail.com>
2024-11-12 11:17:20 -0500

    fixed mobile styling

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 			const response = await fetch("http://localhost:8080/auth_status");
 			const data = await response.json();
 			setIsAuthenticated(data.data);
-			console.log("got");
+			console.log(isAuthenticated);
 		} catch (error) {
 			console.error("Error fetching auth status:", error);
 			setIsAuthenticated(false);
@@ -26,16 +26,16 @@ function App() {
 
 	return (
 		<div className="App">
-			{isAuthenticated ? (
-        // This only shows if someone is authenticated
+			{!isAuthenticated ? (
+				// This only shows if someone is authenticated
 				<div className="nowplaying">
 					<NowPlaying />
 					<Button text="Logout" />
 				</div>
 			) : (
-        // This only shows if no one is authenticated.
+				// This only shows if no one is authenticated.
 
-        // TODO: Make a takeover component.
+				// TODO: Make a takeover component.
 				<div></div>
 			)}
 		</div>
