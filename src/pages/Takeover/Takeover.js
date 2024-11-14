2518e324bfa869818fa1b7aa8aa660e9eaa85612
Ahadu Kebede <ahadukebede@gmail.com>
2024-11-13 23:56:50 -0500

    spotify page can now redirect to the frontend, more seamless now.

--- src/pages/Takeover/Takeover.js
+++ src/pages/Takeover/Takeover.js
@@ -1,10 +1,15 @@
 import { useCallback, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import LoggedOut from "../../components/LoggedOut/LoggedOut";
 import NowPlaying from "../../components/NowPlaying/NowPlaying";
 import Button from "../../components/Button/Button";
-import './Takeover.css'
+import "./Takeover.css";
 
 export default function Takeover() {
+	// handle token submission
+
+	let [searchParams, setSearchParams] = useSearchParams();
+
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
 
 	const fetchAuthStatus = useCallback(async () => {
@@ -24,6 +29,22 @@ export default function Takeover() {
 	});
 
 	useEffect(() => {
+		const code = searchParams.get("code");
+		if (code !== null) {
+			// If the page gets a code from spotify.
+			console.log(code);
+			const URL = `http://localhost:8080/send_auth_token?code=${code}`;
+			console.log("Submitting code");
+			const response = fetch(URL).then((response) =>
+				console.log(response.text())
+			);
+			
+			// It's unsafe to leave the user with the token still in their omnibar,
+			// this is because every time the user reloads the page after they login they will submit the code to their backend.
+			// This line removes the code from their omnibar after submission.
+			window.location.replace("http://localhost:3001/takeover")
+		}
+		// test before you push this.
 		fetchAuthStatus();
 	}, []);
 	return (
