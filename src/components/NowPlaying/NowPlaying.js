579860adb94eb3d481cc03a654758440e2c8a3bc
Ahadu Kebede <ahadukebede@gmail.com>
2024-11-21 13:02:00 -0500

    Updated to work with the new `/current` endpoint response (StateResponse)

--- src/components/NowPlaying/NowPlaying.js
+++ src/components/NowPlaying/NowPlaying.js
@@ -2,25 +2,33 @@ import "./NowPlaying.css";
 import { useEffect, useState } from "react";
 
 export default function NowPlaying() {
-	const [finishedLoading, SetfinishedLoading] = useState(false);
-	const [connectedUser, setConnectedUser] = useState("ahaduk");
+	const [finishedLoading, SetfinishedLoading] = useState(false); // used to track if fetching the API is finished.
+	const [isPlaying, setIsPlaying] = useState(false); // used to track if the user isn't playing anything
+	const [connectedUser, setConnectedUser] = useState("");
 	const [albumArt, setAlbumArt] = useState("");
-	const [songTitle, setSongTitle] = useState("Starboy");
-	const [songArtist, setSongArtist] = useState("The Weeknd");
-	const [upNext, setUpNext] = useState("Die For You");
+	const [songTitle, setSongTitle] = useState("");
+	const [songArtist, setSongArtist] = useState("");
+	const [upNext, setUpNext] = useState("");
 
 	const setStates = () => {
 		try {
 			console.log(finishedLoading);
 			const response = fetch("http://localhost:8080/current")
 				.then((response) => response.json())
-				.then((data) => {
-					setAlbumArt(data.data[0].albumArt);
-					setSongTitle(data.data[0].title);
-					setSongArtist(data.data[0].artist);
-					setUpNext(data.data[1].title);
+				.then((json) => {
+					if (json.isPlaying == false) {
+						// if nothing is playing
+						console.log("Nothing is playing");
+						setIsPlaying(false);
+					} else {
+						setAlbumArt(json.nowPlaying.albumArt);
+						setSongTitle(json.nowPlaying.title);
+						setSongArtist(json.nowPlaying.artist);
+						setUpNext(json.upNext.title);
+						setIsPlaying(true);
+					}
+					setConnectedUser(json.connectedUser);
 					SetfinishedLoading(true);
-					console.log(finishedLoading);
 				});
 		} catch (error) {
 			console.error("Error fetching auth status:", error);
@@ -32,21 +40,33 @@ export default function NowPlaying() {
 	}, []);
 
 	return (
-		<div className="container glasscontainer">
-			<div className="nowplaying">
+		// TODO: If isPlaying is false, reduce the height of the window to 148px.
+		<div
+			className={`container glasscontainer  ${isPlaying ? "" : "h-[148px]"}`}
+		>
+			<div className={`nowplaying`}>
 				{finishedLoading ? (
-					<>
-						<img src={albumArt} className="albumart" />
-						<span className="nowplayingtext">
+					isPlaying ? (
+						<>
+							<img src={albumArt} className="albumart" />
+							<span className="nowplayingtext">
+								<p>
+									<strong>{connectedUser}</strong> is playing{" "}
+									<strong>{songTitle}</strong> by <strong>{songArtist}</strong>
+								</p>
+								<p className="upnext">
+									Up Next: <strong>{upNext}</strong>
+								</p>
+							</span>
+						</>
+					) : (
+						<>
 							<p>
-								<strong>{connectedUser}</strong> is playing{" "}
-								<strong>{songTitle}</strong> by <strong>{songArtist}</strong>
+								<strong>{connectedUser}</strong> is connected but isn't playing
+								anything right now.
 							</p>
-							<p className="upnext">
-								Up Next: <strong>{upNext}</strong>
-							</p>
-						</span>
-					</>
+						</>
+					)
 				) : (
 					<p>Loading...</p>
 				)}
